6fca690447c322f8ea25112d30f460e77784ac6d
Elvin Garcia <elvingarcia19@gmail.com>
2023-01-11 21:19:28 -0500

    creating card that allows the user to create/update the monthly budget

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 // import './App.css';
 import Header from "./components/Header"
+import AddBudget from "./components/AddBudget"
 
 function App() {
   return (
     <div>
       <Header title={"Pocket Wise"} userName={"Elvin"} budget={"5000"}/>
+      <AddBudget />
     </div>
   );
 }
